ab8e65070c2ac3ee4a0a51afe5d27902b04ca1c3
Ludwig <kmouren@qq.com>
2022-12-24 22:11:34 +0800

    删除无用操作

--- main/Kmeans.py
+++ main/Kmeans.py
@@ -4,20 +4,13 @@ from sklearn.cluster import KMeans
 from sklearn.cluster import DBSCAN
 from Utils.FunC import Dimensionality_reduction
 import matplotlib.pyplot as plt
-from Utils.FunC import normalize
 
 data_train=pd.read_csv('../dataset/ECG5000_TRAIN.tsv',sep='\t',header=None)
 x=np.array(data_train)[:,1:141]
-# xmax=np.max(x,axis=0)
-# xmin=np.min(x,axis=0)
-# x=normalize(x,xmax,xmin,0)
 y=np.array(data_train)[:,0]
 
 data_test=pd.read_csv('../dataset/ECG5000_TEST.tsv',sep='\t',header=None)
 x_test=np.array(data_test)[:,1:141]
-# x_tmax=np.max(x_test,axis=0)
-# x_tmin=np.min(x_test,axis=0)
-# x_test=normalize(x_test,x_tmax,x_tmin,0)
 y_test=np.array(data_test)[:,0]
 
 labels=['class 1','class 2','class 3','class 4','class 5']
